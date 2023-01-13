94a36d8c7ee69f2045b1bc2cfa70044a23ce3201
Rafaela ramos dos santos <112700179+RafaelaRSantos@users.noreply.github.com>
2023-01-13 18:38:27 -0300

    Add files via upload

---
+++ CriacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.criaco.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriacaoApiApplication.class, args);
+	}
+
+}
